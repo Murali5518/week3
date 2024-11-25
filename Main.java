a1b1e4366615245c459df211fec854e1f65cf224
Murali <rockymurali10@gmail.com>
2024-11-25 19:39:03 +0530

    first

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        // This is the main method where the program starts execution
+        System.out.println("Hello, World!"); // Prints "Hello, World!" to the console
+          }
+}
